2aeea9d39b7f53f5797eccc53b31b4f4cbc595af
ADTC <send2adtc@gmail.com>
2021-08-22 01:22:53 +0800

    Some refactoring

--- src/extension.ts
+++ src/extension.ts
@@ -23,16 +23,16 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('extension.viewAllDiffInOneBuffer', async () => {
 		const filename = 'ht' + Math.random().toString(36).slice(2) + '.diff'
 		const path = vscode.workspace.rootPath;
-		const temp = `${path}/.git/${filename}`;
 		// Windows commands ("del" won't work in *nix!)
 		await execute(`cd ${path}/.git && del /Q ht*.diff`);
+		const temp = `${path}/.git/${filename}`;
 		const output: string = await execute(`cd ${path} && git diff`);
 		await writeTemp(temp, output);
 		const uri = vscode.Uri.file(temp);
 		//const doc = await vscode.workspace.openTextDocument(uri);
 		//await vscode.window.showTextDocument(doc, { preview: false });
-		vscode.commands.executeCommand("vscode.openWith", uri, "diffViewer",
-					       { preview: true, preserveFocus: false });
+		await vscode.commands.executeCommand("vscode.openWith", uri, "diffViewer",
+				{ preview: true, preserveFocus: false });
 	});
 
 	context.subscriptions.push(disposable);
